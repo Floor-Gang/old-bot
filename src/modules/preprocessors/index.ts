5003529e250c8615c9a9a1c209eca58bc29df50f
Dylan Hackworth <dhpf@pm.me>
2020-06-16 17:23:30 -0500

    Disabled GameChanger, server is unresponsive

--- src/modules/preprocessors/index.ts
+++ src/modules/preprocessors/index.ts
@@ -1,7 +1,8 @@
 import { Messages } from "./Messages";
 import { Suggestions } from "./Suggestions";
 import { ChannelLink } from "./ChannelLink";
-import { GameChanger } from "./GameChanger";
+// import { GameChanger } from "./GameChanger";
+import { GrowingLobby } from "./GrowingLobby";
 
 /**
  * When a new Preprocessor class is made it should be appended to this array.
@@ -10,5 +11,6 @@ export const preprocessors = [
   new Messages(),
   new Suggestions(),
   new ChannelLink(),
-  new GameChanger()
+  // new GameChanger(),
+  new GrowingLobby()
 ];
