ce33285a54693cb7ef8779d0bbea8abfc7dbcd94
Dylan Hackworth <dhpf@pm.me>
2020-06-17 14:45:14 -0500

    Updated n-word detection

--- src/util/Censorship.ts
+++ src/util/Censorship.ts
@@ -10,7 +10,7 @@ export class Censorship {
    */
   public static hasNWord(context: string): boolean {
     const match = context.match(
-      /(n([i!1])gg?(er|a)?\s|ni(gg|bb|qq)ers|ni(bb?|qq?)(er|a)?|niU+1F171U+1F171?(er|a)?|n\s?([i1])\s?g\s?g\s?([e3])\s?r|n\s?i\s?g\s?g\s?a|niger(?!ia)|)/g
+      /(n([i!1])gg?(er|a)?\s|ni(gg|bb|qq)ers|ni(bb?|qq?)(er|a)?\W|niU+1F171U+1F171?(er|a)?|n\s?([i1])\s?g\s?g\s?([e3])\s?r|n\s?i\s?g\s?g\s?a|niger(?!ia)|)/g
     );
 
     if (!match)
