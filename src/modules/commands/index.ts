f58c1840d5d113043235275e84606e0e8dd2fc4b
Dylan Hackworth <dhpf@pm.me>
2020-06-30 14:11:21 -0500

    Removed Vote command

--- src/modules/commands/index.ts
+++ src/modules/commands/index.ts
@@ -3,7 +3,6 @@ import type { Command } from "../../core/models/Command";
 import { Who } from "./Who";
 import { Admin } from "./Admin";
 import { Rules } from "./Rules";
-import { Vote } from "./Vote";
 import { LinkChannel } from "./LinkChannel";
 import { Flux } from "./Flux";
 
@@ -15,7 +14,6 @@ export const commands: Command[] = [
   new Ping(),
   new Who(),
   new Rules(),
-  new Vote(),
   new LinkChannel(),
   new Flux(),
 ];
